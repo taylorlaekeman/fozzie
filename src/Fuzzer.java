7add3add302dbd1fd3b354a92505b66acb6740cc
taylor <taylor.laekeman@gmail.com>
2017-06-23 16:14:41 -0400

    added input checking for command line argument

--- src/Fuzzer.java
+++ src/Fuzzer.java
@@ -4,7 +4,7 @@ class Fuzzer {
   private static final int NUM_COMMANDS_INDEX = 0;
 
   public static void main(String[] args) {
-    int commands = Integer.parseInt(args[NUM_COMMANDS_INDEX]);
+    int commands = (args.length > 0) ? Integer.parseInt(args[NUM_COMMANDS_INDEX]) : 1;
     Command command;
     for (int i = 0; i < commands; i++) {
       command = Command.makeRandom(i);
