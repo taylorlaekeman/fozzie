2bf04804db5f4191365f91d30c529c9f24118616
taylor <taylor.laekeman@gmail.com>
2017-07-17 20:59:54 -0400

    implemented invalid pcmd command generation

--- src/commands/PcmdCommandBuilder.java
+++ src/commands/PcmdCommandBuilder.java
@@ -27,10 +27,23 @@ public class PcmdCommandBuilder extends CommandBuilder {
 		);
 	}
 
+	@Override
+	protected String makeInvalid(int sequenceNumber) {
+		return String.format(
+			"AT*PCMD=%d,%d,%f,%f,%f,%f<CR>",
+			sequenceNumber,
+			getInvalidFlag(),
+			getInvalidRoll(),
+			getInvalidPitch(),
+			getInvalidGaz(),
+			getInvalidYaw()
+		);
+	}
+
 	protected int getValidFlag() {
-    int absoluteControlEnableBit = CommandFactory.RANDOM.nextInt(2);
+    int absoluteControlEnableBit = CommandFactory.RANDOM.nextInt(2); 
     int combinedYawEnableBit = CommandFactory.RANDOM.nextInt(2);
-    int progressiveCommandsEnableBit = CommandFactory.RANDOM.nextInt(2);
+    int progressiveCommandsEnableBit = 1;
     return (absoluteControlEnableBit << 2) | (combinedYawEnableBit << 1) | progressiveCommandsEnableBit;
 	}
 
@@ -48,5 +61,39 @@ public class PcmdCommandBuilder extends CommandBuilder {
 
 	protected float getValidYaw() {
 		return (CommandFactory.RANDOM.nextFloat() * 2) - 1;
+	}
+
+	protected int getInvalidFlag() {
+		int flag = CommandFactory.RANDOM.nextInt(Integer.MAX_VALUE);
+		flag = flag | 1; // progressive commands enable bit = 1
+		return flag;
+	}
+
+	protected float getInvalidRoll() {
+		float roll = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			roll *= -1;
+		return roll;
+	}
+
+	protected float getInvalidPitch() {
+		float pitch = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			pitch *= -1;
+		return pitch;
+	}
+
+	protected float getInvalidGaz() {
+		float gaz = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			gaz *= -1;
+		return gaz;
+	}
+
+	protected float getInvalidYaw() {
+		float yaw = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			yaw *= -1;
+		return yaw;
 	}
 }
--- src/commands/PcmdMagCommandBuilder.java
+++ src/commands/PcmdMagCommandBuilder.java
@@ -29,11 +29,40 @@ public class PcmdMagCommandBuilder extends PcmdCommandBuilder {
 		);
 	}
 
+	@Override
+	protected String makeInvalid(int sequenceNumber) {
+		return String.format(
+			"AT*PCMD_MAG=%d,%d,%f,%f,%f,%f,%f,%f<CR>",
+			sequenceNumber,
+			getInvalidFlag(),
+			getInvalidRoll(),
+			getInvalidPitch(),
+			getInvalidGaz(),
+			getInvalidYaw(),
+			getInvalidPsi(),
+			getInvalidPsiAccuracy()
+		);
+	}
+
 	protected float getValidPsi() {
 		return (CommandFactory.RANDOM.nextFloat() * 2) - 1;
 	}
 
 	protected float getValidPsiAccuracy() {
 		return (CommandFactory.RANDOM.nextFloat() * 2) - 1;
+	}
+
+	protected float getInvalidPsi() {
+		float psi = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			psi *= -1;
+		return psi;
+	}
+
+	protected float getInvalidPsiAccuracy() {
+		float psiAccuracy = (CommandFactory.RANDOM.nextFloat() + 1);
+		if (CommandFactory.RANDOM.nextBoolean())
+			psiAccuracy *= -1;
+		return psiAccuracy;
 	}
 }
